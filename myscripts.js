507bef3f5d1dba5713e787b88d4e31cbd9973356
Sana Javed <sanajaved7@gmail.com>
2015-04-19 16:37:09 -0400

    Updated CSS, added jQuery, added links

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,9 @@
 $("button").on("click", function() { alert("Thanks! Expect an email from me soon!")});
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").toggle("slow");
+    });
+});
+</script>
